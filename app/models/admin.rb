57a37d51190d779c3247fc17ff0517366df20f4a
belly <abdelrhmanalaa39@gmail.com>
2017-02-18 17:25:37 +0000

    bug fixes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,3 @@
 class Admin < ActiveRecord::Base
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable, :rememberable
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable
 end
